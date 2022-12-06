9deff71761c3c1e3e99123627c7d82f7c3b99ec7
Joseildo <joseildo.sfc@gmail.com>
2022-12-05 22:17:15 -0300

    Inserindo e buscando registros no banco de dados via node.js/express

--- app.js
+++ app.js
@@ -1,19 +1,15 @@
 const express = require("express");
 const connection = require("./db");
+const bodyParser = require("body-parser");
 
 
 const app = express();
 
 
-app.get("/logs", (req, res) => {
-    const find = connection.query(`SELECT * FROM users`, (err, rows, field) => {
-        err ? res.send(err) : res.send(rows);
-    });
-});
-
-
 app.set("view engine", "hbs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 app.get("/", (req, res) => {
@@ -28,6 +24,30 @@ app.get("/nossaMissao", (req, res) => {
 app.get("/cursos", (req, res) => {
     res.render("cursos");
 });
+
+
+
+app.post("/register", (req, res) => {
+
+    let namePerson = req.body.name;
+    let emailPerson = req.body.email;
+    let inserts;
+
+    if (namePerson && emailPerson) {
+        inserts = connection.query(`INSERT INTO users (name, email) VALUES ('${namePerson}','${emailPerson}')`);
+        console.log({ inserts });
+    }
+    inserts ? res.send(`Dados cadastrados, Nome: ${namePerson}.`) : res.send("Dados nao cadastrados.");
+});
+
+
+app.get("/logs", (req, res) => {
+    const find = connection.query(`SELECT * FROM users`, (err, rows, field) => {
+        err ? res.send(err) : res.send(rows);
+    });
+});
+
+
 
 app.listen(8080, (err) => {
     err ? console.log(err) : console.log("Server inicializado...");
